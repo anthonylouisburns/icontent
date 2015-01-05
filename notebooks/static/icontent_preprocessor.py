9ad8474c25bc3ed44e3a20f572c1a96602a6af18
anthonylouisburns <burnstony@gmail.com>
2015-01-05 21:53:42 +0000

    add all

--- notebooks/static/icontent_preprocessor.py
+++ notebooks/static/icontent_preprocessor.py
@@ -2,21 +2,11 @@ from IPython.nbconvert.preprocessors import *
 import json
 import itertools
 
-class ItbPreprocessor(Preprocessor):
-    """docstring for CheesePreprocessor"""
+class IContentPreprocessor(Preprocessor):
     def preprocess(self, nb, resources):
         print('preprocess')
         self.setVariables(nb, resources)
         return nb, resources
-
-    def preprocess_cell(self, cell, resources, index):
-        """
-        Adds bold 'cheese' to the start of every markdown cell.
-        """
-
-        if 'source' in cell and cell.cell_type == "markdown":
-            cell.source = '**cheese cheese cheese cheese cheese cheese cheese cheese cheese cheese cheese cheese cheese cheese cheese cheese** ' + cell.source
-        return cell, resources
 
     def setVariables(self, nb, resources):
         print('setVariables')
@@ -24,7 +14,7 @@ class ItbPreprocessor(Preprocessor):
         sourcelist = [cell["source"].split("\n") for cell in nb["cells"] if cell["cell_type"] == 'markdown']
         allsource = list(itertools.chain(*sourcelist))
 
-        resources['template'] = "/notebooks/static/itb.tpl"
+        resources['template'] = "/notebooks/static/icontent.tpl"
         resources['css'] = "/css/home.css"
         resources['sideleft'] = "sideleft.html"
         resources['links'] = [["Anthony Louis Burns", "http://www.anthonylouisburns.com", "pad0"], ["IContent", "http://www.anthonylouisburns.com/icontent/IContent.html", "pad0"], ["IContent - github", "https://github.com/anthonylouisburns/icontent/", "pad0"], ["Anthony Louis Burn - LinkedIn", "https://www.linkedin.com/in/anthonylouisburns", "pad0"]]
--- notebooks/static/set_const.py
+++ notebooks/static/set_const.py
@@ -1,5 +1,5 @@
 c = get_config()
 
 #Export all the notebooks in the current directory to the sphinx_howto format.
-c.Exporter.template_file = 'itb'
-c.Exporter.preprocessors = ['itb_preprocessor.ItbPreprocessor']
+c.Exporter.template_file = 'icontent'
+c.Exporter.preprocessors = ['icontent_preprocessor.IContentPreprocessor']
